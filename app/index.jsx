114cae961d0157e20f298b31cadedd6351bc8ed6
Joe Attardi <joe@attardi.net>
2016-08-10 17:43:43 -0400

    fix favicon

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import 'file?name=logo.png!./images/logo.png';
 import './scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
