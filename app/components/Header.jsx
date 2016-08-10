15baef721596d8291fe95f0811e5b7d0998b0dc9
Joe Attardi <joe@attardi.net>
2016-08-10 17:43:38 -0400

    make header smaller

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../images/logo.png';
 export default function Header(props) {
   return (
     <header>
-      <img src={logo} width="40" height="40" />
+      <img src={logo} width="25" height="25" />
       <h1>Kolinahr</h1>
     </header>
   );
