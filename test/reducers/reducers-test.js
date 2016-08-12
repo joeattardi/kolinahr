18766721949d2f462939b0580e3506b2264f2967
Joe Attardi <joe@attardi.net>
2016-08-11 20:12:03 -0400

    Add reducer tests

--- test/reducers/reducers-test.js
+++ test/reducers/reducers-test.js
@@ -1,5 +1,6 @@
-import { ADD_CARD } from '../../app/actions/types';
-import { cardsReducer } from '../../app/reducers';
+import { VIEW_MODE, EDIT_MODE } from '../../app/constants';
+import { ADD_CARD, EDIT_TITLE, EDIT_TITLE_SAVE, EDIT_TITLE_CANCEL } from '../../app/actions/types';
+import { cardsReducer, titleReducer, titleModeReducer } from '../../app/reducers';
 
 describe('Reducers', () => {
   describe('Card Reducer', () => {
@@ -18,6 +19,51 @@ describe('Reducers', () => {
       };
 
       expect(cardsReducer(undefined, action)).toEqual(expected);
+    });
+  });
+
+  describe('Title Reducer', () => {
+    it('updates the title when the edit is saved', () => {
+      const action = {
+        type: EDIT_TITLE_SAVE,
+        payload: 'new title'
+      };
+
+      expect(titleReducer('old title', action)).toEqual('new title');
+    });
+
+    it('does not update the title when the edit is cancelled', () => {
+      const action = {
+        type: EDIT_TITLE_CANCEL
+      };
+
+      expect(titleReducer('old title', action)).toEqual('old title');
+    });
+  });
+
+  describe('Title Mode Reducer', () => {
+    it('changes to edit mode on edit', () => {
+      const action = {
+        type: EDIT_TITLE
+      };
+
+      expect(titleModeReducer(VIEW_MODE, action)).toEqual(EDIT_MODE);
+    });
+
+    it('changes to view mode on cancel', () => {
+      const action = {
+        type: EDIT_TITLE_CANCEL
+      };
+
+      expect(titleModeReducer(EDIT_MODE, action)).toEqual(VIEW_MODE);
+    });
+
+    it('changes to view mode on save', () => {
+      const action = {
+        type: EDIT_TITLE_SAVE
+      };
+
+      expect(titleModeReducer(EDIT_MODE, action)).toEqual(VIEW_MODE);
     });
   });
 });
