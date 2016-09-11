42803a41a83ccd5910fb6fd7b5fb25f3894825d2
Joe Attardi <joe@attardi.net>
2016-09-10 23:05:44 -0400

    style login page

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default class Login extends React.Component {
   render() {
     return (
-      <a href="/auth/github">Log in with GitHub</a>
+      <div id="login-container">
+        <h1><img src="logo.png" width="50" height="50" alt="Logo" />Kolinahr</h1>
+        <a id="login-button" href="/auth/github">
+          <i className="fa fa-2x fa-github" /> Log in with GitHub
+        </a>
+      </div>
     );
   }
 }
